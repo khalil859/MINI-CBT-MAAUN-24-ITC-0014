73caab01c3d33d64a67af260de936c2c3f7bd794
MAAUN/24/ITC/0014 <ibrahimbukar2622@gmail.com>
2026-03-20 18:10:27 -0400

    Create models.py

---
+++ models.py
@@ -0,0 +1,8 @@
+1.Models .py
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+    def check_answer(self, user_answer):
+        return user_answer.lower() == self.answer.lower()
